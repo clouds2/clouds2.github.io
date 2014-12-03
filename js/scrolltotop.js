c5911dbf09fe94aa711b0b6f017d69aeb79aa6be
seoulrain <seoulrain@gmail.com>
2014-12-03 14:04:46 +0900

    asdf

--- js/scrolltotop.js
+++ js/scrolltotop.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	
 	//Check to see if the window is top if not then display button
 	$(window).scroll(function(){
-		if ($(this).scrollTop() > 500) {
+		if ($(this).scrollTop() > 100) {
 			$('.scrollToTop').fadeIn();
 		} else {
 			$('.scrollToTop').fadeOut();
