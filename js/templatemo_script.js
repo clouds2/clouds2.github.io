b3a9a1a6d99d61c17d079200703925565d86f8f5
seoulrain <seoulrain@gmail.com>
2014-12-03 12:03:01 +0900

    asdf

--- js/templatemo_script.js
+++ js/templatemo_script.js
@@ -2,23 +2,6 @@
 
 jQuery(document).ready(function()
 {
-    //mobile menu show hide
-    jQuery("header #mobile_menu_list ul").hide();
-    jQuery("#mobile_menu").click(function(){
-        jQuery("header #mobile_menu_list ul").slideToggle();
-        return false;
-    });
-    jQuery("header #mobile_menu_list ul li a").click(function(){
-        jQuery("header #mobile_menu_list ul").slideUp();
-    });
-    //single page nav
-    jQuery("header ul").singlePageNav({offset: jQuery('header').outerHeight()});
-    //open scroll function
-    jQuery("html, body").animate({ scrollTop: 50 }, 0, function(){
-        jQuery(this).animate({ scrollTop: 0 },1000);
-    });
-    //call flex slider function
-    jQuery('#main-slider').flexslider();
     //scroll to top
     jQuery(window).scroll(function(){
         if(jQuery(this).scrollTop() > 100){
